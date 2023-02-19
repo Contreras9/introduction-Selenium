720c1d7333f5889d9312b6d425975dca508dd0e6
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-19 15:35:35 -0600

    Driver setup

--- src/main/java/SeleniumIntro.java
+++ src/main/java/SeleniumIntro.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SeleniumIntro {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+    public static void main(String[] args) {
+
+        // Invoking Browser - class methods
+        // Chrome - ChromeDriver -> methods close get
+        // Firefox - FirefoxDriver -> methods close get
+        // Safari - Safari Driver -> methods close get
+        // WebDriver close get
+        // WebDriver methods or class methods
+        // ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+
+    }
 }
