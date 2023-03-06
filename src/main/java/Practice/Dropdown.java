1fa36ceb156ff16a729e04d6eacd234cc8419cf6
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-06 17:12:19 -0600

    Handle Dynamic Dropdowns with WebDriver API

---
+++ src/main/java/Practice/Dropdown.java
@@ -0,0 +1,19 @@
+package Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dropdown {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+    }
+}
