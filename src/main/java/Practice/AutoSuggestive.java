23768e1f438cfd45371c0c0971c1c02b1f005655
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-06 22:18:44 -0600

    Handling AutoSuggestive Dropdowns Using Selenium

---
+++ src/main/java/Practice/AutoSuggestive.java
@@ -0,0 +1,28 @@
+package Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class AutoSuggestive {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(3000);
+        List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+
+        for (WebElement option : options) {
+            if (option.getText().equalsIgnoreCase("India")) {
+                option.click();
+                break;
+            }
+        }
+
+    }
+}
