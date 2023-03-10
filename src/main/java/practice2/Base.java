b0cdc1fb6126d7f7bf54787e943585d81d7b60b6
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-10 01:02:34 -0600

    Building Programming Logic to Process Items in Array for Cart

--- src/main/java/practice2/Base.java
+++ src/main/java/practice2/Base.java
@@ -5,22 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Base {
     public static void main(String[] args) {
         System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
         WebDriver driver = new ChromeDriver();
-        String[] products = {"Cucumber", "Broccoli"};
+        String[] veggiesNeeded = {"Cucumber", "Broccoli", "Beetroot"};
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 
         List<WebElement> veggies = driver.findElements(By.cssSelector("h4.product-name"));
 
         for (int i = 0; i < veggies.size(); i++) {
             String veggie = veggies.get(i).getText();
-            if (veggie.contains("Cucumber")) {
+            List itemsNeededList = Arrays.asList(veggiesNeeded);
+            if (itemsNeededList.contains(veggie)) {
                 driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-                break;
             }
         }
     }
