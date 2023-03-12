49ba9cb70c330354876b57460b3dfc3ec93ddc9a
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-12 00:12:00 -0600

    Practical Examples of Explicit Waits

--- src/main/java/practice2/Base.java
+++ src/main/java/practice2/Base.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
@@ -23,7 +25,8 @@ public class Base {
 
         System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         String[] veggiesNeeded = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
 
@@ -32,8 +35,12 @@ public class Base {
         addItems(driver, veggiesNeeded);
         driver.findElement(By.cssSelector("img[alt='Cart']")).click();
         driver.findElement(By.xpath("//button[contains(.,'PROCEED TO CHECKOUT')]")).click();
+        wdw.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
         driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
         driver.findElement(By.cssSelector("button.promoBtn")).click();
+        // explicit wait
+        wdw.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+
         System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
     }
 
