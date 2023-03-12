0fa216276f6104c1cc6ae576204a4da111a27d41
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-12 01:54:40 -0600

    Building Customized Methods Using Fluent Wait

---
+++ src/main/java/practice2/FluentWaitTest.java
@@ -0,0 +1,42 @@
+package practice2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class FluentWaitTest {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable notifications");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        options.merge(capabilities);
+
+        System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.cssSelector("[id='finish'] h4"));
+            }
+        });
+
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed());
+
+    }
+}
