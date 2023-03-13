2327bed725964bbad2d0c6146e5ae679ac2c209f
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-12 23:31:30 -0500

    Window Handle Concepts – Real-Time Example

--- src/main/java/amazonSite_RahulSite/ActionsDemo.java
+++ src/main/java/amazonSite_RahulSite/ActionsDemo.java
@@ -1,6 +1,5 @@
-package amazonSite;
+package amazonSite_RahulSite;
 
-import net.bytebuddy.implementation.bytecode.ShiftLeft;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
---
+++ src/main/java/amazonSite_RahulSite/WindowHandles.java
@@ -0,0 +1,32 @@
+package amazonSite_RahulSite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class WindowHandles {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable notifications");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        options.merge(capabilities);
+
+        System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+        driver.findElement(By.cssSelector("a.blinkingText")).click();
+        Set<String> windows = driver.getWindowHandles();   // [parentId, childId]
+        Iterator<String> it = windows.iterator();
+        String parentId = it.next();
+        String childId = it.next();
+        driver.switchTo().window(childId);
+        System.out.println(driver.findElement(By.cssSelector("p.im-para.red")).getText());
+    }
+}
