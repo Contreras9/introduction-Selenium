e4dc0f741bc52c447a74b21fc93c54e536544030
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-12 23:50:09 -0500

    Live Example on Working with Child Windows

--- src/main/java/amazonSite_RahulSite/WindowHandles.java
+++ src/main/java/amazonSite_RahulSite/WindowHandles.java
@@ -28,5 +28,9 @@ public class WindowHandles {
         String childId = it.next();
         driver.switchTo().window(childId);
         System.out.println(driver.findElement(By.cssSelector("p.im-para.red")).getText());
+
+        String emailId = driver.findElement(By.cssSelector("p.im-para.red")).getText().split("at")[1].trim().split(" ")[0];
+        driver.switchTo().window(parentId);
+        driver.findElement(By.id("username")).sendKeys(emailId);
     }
 }
