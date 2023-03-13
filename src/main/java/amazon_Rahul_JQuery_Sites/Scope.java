5a77294a50d65576436ecfdbc82d4d7998b58017
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-13 15:40:02 -0500

    Exercise 1.1 - Limiting WebDriver Scope

--- src/main/java/amazon_Rahul_JQuery_Sites/Scope.java
+++ src/main/java/amazon_Rahul_JQuery_Sites/Scope.java
@@ -19,9 +19,20 @@ public class Scope {
         System.setProperty("webDriver.chrome.driver", "/Users/hamzahcontreras/Development/Java/SeleniumFundamentals/chromedriver_mac_arm64");
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        // 1.Give me the count of links on the page.
         System.out.println(driver.findElements(By.tagName("a")).size());
 
+        // 2. Count of footer section
         WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
         System.out.println(footerDriver.findElements(By.tagName("a")).size());
+
+        WebElement columnDriver = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+        System.out.println(columnDriver.findElements(By.tagName("a")).size());
+
+        // Click on each link in the column and check if the pages are opening.
+        for (int i = 1; i < columnDriver.findElements(By.tagName("a")).size(); i++) {
+            columnDriver.findElements(By.tagName("a")).get(i).click();
+        }
+
     }
 }
